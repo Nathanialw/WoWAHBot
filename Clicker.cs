28a332535316af20fba56204d1108c2e4366be5b
Nathanial Walton <nathanial28@gmail.com>
2021-08-22 23:18:50 -0600

    Add project files.

---
+++ Clicker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using AutoIt;
+
+
+namespace ClickControl
+{
+    static class Clicker
+    {
+        //data
+        private static readonly Random r = new Random();
+
+        public static int variance = 50;
+        public static int iterations = 0;
+        public static string windowTitle = "World of Warcraft";
+        public static string spamKey = "+{F12}";
+        public static int Slider_Pos;
+        public static bool state = false;
+
+        //methods
+        public static async void RunClickerAsync()
+        {
+            state = true;
+            while (state)
+            {
+                _ = AutoItX.ControlSend(windowTitle, "", "", spamKey);
+                await Task.Delay(DelayCalc());
+            }
+        }
+
+        private static int DelayCalc()
+        {
+            int a = Slider_Pos - variance;
+            int b = Slider_Pos + variance;
+            if (a < 1) { a = 1; };              //in case of zero or negative
+            return r.Next(a, b);
+        }
+    }
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ClickControl;
+
+
+namespace test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Top clicker portion
+        private void Button_Start(object sender, RoutedEventArgs e)
+        {
+            start.IsEnabled = false;
+            stop.IsEnabled = true;
+            Clicker.RunClickerAsync();
+        }
+        private void Button_Stop(object sender, RoutedEventArgs e)
+        {
+            stop.IsEnabled = false;
+            start.IsEnabled = true;
+            Clicker.state = false;
+        }
+        private void Delay_Slider(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Clicker.Slider_Pos = Convert.ToInt32(e.NewValue);
+        }
+        private void variance_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Clicker.variance = Convert.ToInt32(e.NewValue);
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Clicker.spamKey = Spam_Key.Text;
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Clicker.windowTitle = Window_Title.Text;
+        }
+
+        //Bottom bot portion
+
+    }
+}
+
+
+
+
